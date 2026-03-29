45035e087aa6f63d060565c72913ea8873427a49
Nguyen Chi Cong <nguycc04@heiway.net>
2026-03-29 14:58:26 +0700

    fature: addd swagger

--- server/src/modules/testRun/testRun.routes.js
+++ server/src/modules/testRun/testRun.routes.js
@@ -5,7 +5,7 @@ const authMiddleware = require("../../middleware/auth.middleware");
 const testRunController = require("./testRun.controller");
 
 const router = express.Router();
-
+router.use(authMiddleware);
 /**
  * @swagger
  * tags:
@@ -120,5 +120,5 @@ router.get("/:id", testRunController.getTestRunDetail);
  *         description: Unauthorized
  */
 router.post("/", testRunController.createTestRun);
-
+router.post("/:id/replay", testRunController.replayTestRun);
 module.exports = router;
